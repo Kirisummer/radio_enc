989f7e8edabe82ef0d0965e1c6e6270cc8906185
LyraLover <ediktrikoz@gmail.com>
2022-05-28 04:13:44 +0300

    Add basic benchmarking abilities

--- main.py
+++ main.py
@@ -4,28 +4,64 @@ from transmission import write_file
 from ciphers import Rsa, Aes, Salsa20, SHA256
 from encryption import encrypt, decrypt
 
+import os
 import sys
+import timeit
 
-def main(filename, seconds):
+def main(seconds):
     audioconf.init()
 
-    rsa = Rsa(Rsa.KeyLen.Rsa2048, SHA256)
-    key = rsa.generate_key()
-    ciphers = Aes(Aes.KeyLen.AES_256), Salsa20(Salsa20.KeyLen.B32)
-
     data = record_sound(seconds)
-    enc_data = encrypt(rsa, key, rsa.hash_key, ciphers, data)
-    dec_data = decrypt(rsa, key, rsa.hash_key, ciphers, enc_data)
 
-    write_file(data, filename + '.wav')
-    write_file(enc_data, filename + '.enc.wav')
-    write_file(dec_data, filename + '.dec.wav')
+    rsa = Rsa(Rsa.KeyLen.Rsa2048, SHA256)
+    benchmark(data, rsa, rsa.generate_key(), rsa.hash_key,
+            (Aes(Aes.KeyLen.AES_256), Salsa20(Salsa20.KeyLen.B32)),
+            'RSA_Aes256_Salsa')
+    benchmark(data, rsa, rsa.generate_key(), rsa.hash_key,
+            (Aes(Aes.KeyLen.AES_128), Salsa20(Salsa20.KeyLen.B32)),
+            'RSA_Aes128_Salsa')
+    benchmark(data, rsa, rsa.generate_key(), rsa.hash_key,
+            (Salsa20(Salsa20.KeyLen.B32), Aes(Aes.KeyLen.AES_256)),
+            'RSA_Salsa_Aes256')
+    benchmark(data, rsa, rsa.generate_key(), rsa.hash_key,
+            (Salsa20(Salsa20.KeyLen.B32), Aes(Aes.KeyLen.AES_128)),
+            'RSA_Salsa_Aes128')
 
     audioconf.terminate()
-    zeroes, ones = count_bin(enc_data)
 
+def benchmark(data, key_cipher, key, key_hash, ciphers, out_name):
+    print('#', out_name, '###########################################')
+
+    # Час
+    timer('Encryption', lambda: encrypt(key_cipher, key, key_hash, ciphers, data), 10)
+    enc_data = encrypt(key_cipher, key, key_hash, ciphers, data)
+    timer('Decryption', lambda: decrypt(key_cipher, key, key_hash, ciphers, enc_data), 10)
+    dec_data = decrypt(key_cipher, key, key_hash, ciphers, enc_data)
+
+    # Запис до файлів
+    write_files(out_name, data, enc_data, dec_data)
+
+    # Розмір даних
+    print(f'Data size:', len(data), sep='\t')
+    print(f'Encrypted size:', len(enc_data), sep='\t')
+    print(f'Increase:', len(enc_data) * 100 / len(data) - 100, sep='\t', end='%\n')
+
+    # Криптостійкість (TODO: однорідність)
+    zeroes, ones = count_bin(enc_data)
     print(f'0: {zeroes}, 1: {ones}')
     print(f'data == dec_data: {data == dec_data}')
+
+    print('\n')
+
+def timer(label, action, times):
+    avg = timeit.Timer(action).timeit(number=times) / times
+    print(f'{label} over {times} time(s): {avg}s')
+
+def write_files(out_name, data, enc_data, dec_data):
+    os.mkdir(out_name)
+    write_file(data,     f'{out_name}/rec.wav')
+    write_file(enc_data, f'{out_name}/enc.wav')
+    write_file(dec_data, f'{out_name}/dev.wav')
 
 def count_bin(data):
     zeroes = ones = 0
@@ -38,4 +74,4 @@ def count_bin(data):
     return zeroes, ones
 
 if __name__ == '__main__':
-    main(sys.argv[1], int(sys.argv[2]))
+    main(int(sys.argv[1]))
